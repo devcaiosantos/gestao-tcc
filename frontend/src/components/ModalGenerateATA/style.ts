957fe51ea63876f1b25d983301672c6d580dc413
Caio Luiz dos Santos <devcaiosantos@gmail.com>
2025-01-08 19:11:36 -0300

    #43 - modal generate ATA

---
+++ frontend/src/components/ModalGenerateATA/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ContainerModal = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+export const SelectContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`;
+
+export const LabelSelect = styled.div`
+    font-size: 1.2rem;
+    font-weight: 600;
+`
